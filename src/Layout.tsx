9debd6c21b2cf43b6cdd0a0ac0ddc731b94de9b5
Ian Matthew Sobule <imsobule@gmail.com>
2022-01-08 14:39:42 -0600

    remove lingering diplsay font classs

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="mx-auto h-full min-h-screen items-center my-20 font-display flex flex-col">
+      <main className="mx-auto h-full min-h-screen items-center my-20 flex flex-col">
         {children}
       </main>
     </>
